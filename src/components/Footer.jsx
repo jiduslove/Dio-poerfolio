f1265f507e6c888ecb5c37c095779b5abeea8873
jiduslove <oondayr@gamil.com>
2023-04-04 19:33:00 +0900

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white max-w-screen-xl mx-auto flex justify-end items-center py-1 md:py-2">
+      2023, Designed by, Dio
+    </footer>
+  );
+};
+
+export default Footer;
